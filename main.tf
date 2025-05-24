7b89068e29e9b1c870ed1bf2d35fc2aa474b28df
JacobPEvans <20714140+JacobPEvans@users.noreply.github.com>
2025-05-24 14:50:02 -0400

    Unmodified copilot Splunk tf
    Signed-off-by: JacobPEvans <20714140+JacobPEvans@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,2 +1,33 @@
-# This is where we'll define our Proxmox resources
-# Initially empty, will be used for VM definitions later
+
+resource "proxmox_vm_qemu" "splunk" {
+  name        = "splunk"
+  target_node = var.proxmox_node
+
+  # VM hardware
+  cores       = 4
+  sockets     = 1
+  memory      = 8192
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+
+  disk {
+    slot     = 0
+    size     = "50G"
+    type     = "scsi"
+    storage  = "local-lvm" # Change if your storage is different
+    iothread = true
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  # Attach the ISO for installation
+  cdrom {
+    file = "local:iso/ubuntu-24.04.1-live-server-amd64.iso"
+  }
+
+  # Optional: set boot order to boot from CD first
+  boot = "cd"
+}
